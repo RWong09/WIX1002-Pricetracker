bd605baa3b45df84335bd6fe6b5dcb3f05855a0f
TITUS TAN YU FAN <148965408+Titustyf@users.noreply.github.com>
2024-01-09 00:37:02 +0800

    Add files via upload

---
+++ finalisedPriceTracker/ChangePass.java
@@ -0,0 +1,233 @@
+package PriceTracker;
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.showMessageDialog;
+import javax.swing.JPanel;
+
+public class ChangePass extends javax.swing.JFrame {
+
+    private String Username;
+
+    ChangePass(String Username) {
+        this.Username = Username;
+        initComponents();
+    }
+
+    class PriceTracker extends JPanel {
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g;
+            int width = getWidth();
+            int height = getHeight();
+
+            Color color1 = new Color(219, 215, 210);
+            Color color2 = new Color(176, 196, 222);
+            GradientPaint gp = new GradientPaint(0, 0, color1, 180, height, color2);
+            g2d.setPaint(gp);
+            g2d.fillRect(0, 0, width, height);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new PriceTracker();
+        Changepass = new javax.swing.JLabel();
+        NewPass = new javax.swing.JLabel();
+        newpass = new javax.swing.JPasswordField();
+        confirmpass = new javax.swing.JLabel();
+        connewpass = new javax.swing.JPasswordField();
+        Confirm = new javax.swing.JButton();
+        Back = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setPreferredSize(new java.awt.Dimension(500, 400));
+
+        Changepass.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        Changepass.setForeground(new java.awt.Color(29, 41, 81));
+        Changepass.setText("Change Password");
+
+        NewPass.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        NewPass.setForeground(new java.awt.Color(29, 41, 81));
+        NewPass.setText("New Password:");
+
+        confirmpass.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        confirmpass.setForeground(new java.awt.Color(29, 41, 81));
+        confirmpass.setText("Confirm New Password:");
+
+        Confirm.setForeground(new java.awt.Color(29, 41, 81));
+        Confirm.setText("Confirm");
+        Confirm.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ConfirmActionPerformed(evt);
+            }
+        });
+
+        Back.setForeground(new java.awt.Color(29, 41, 81));
+        Back.setText("<< Back");
+        Back.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BackActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(169, 169, 169)
+                .addComponent(Changepass, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(141, 141, 141))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addComponent(connewpass))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(NewPass, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGap(268, 268, 268))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(confirmpass, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGap(204, 204, 204))
+                            .addComponent(newpass)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addGap(199, 199, 199)
+                        .addComponent(Confirm, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(94, 94, 94)
+                        .addComponent(Back, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addGap(67, 67, 67))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addComponent(Changepass, javax.swing.GroupLayout.DEFAULT_SIZE, 46, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(NewPass, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(newpass)
+                .addGap(18, 18, 18)
+                .addComponent(confirmpass, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(connewpass)
+                .addGap(26, 26, 26)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Confirm, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(Back, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(165, 165, 165))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 506, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void BackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BackActionPerformed
+        AccSetting Acc = new AccSetting(Username);
+        Acc.setVisible(true);
+        Acc.pack();
+        Acc.setLocationRelativeTo(null);
+        this.dispose();
+    }//GEN-LAST:event_BackActionPerformed
+
+    private void ConfirmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ConfirmActionPerformed
+        String Newpassword;
+
+        String url = "jdbc:mysql://localhost:3306/pricetracker";
+        String user = "root";
+        String password = "";
+
+        try {
+            Connection sqlconnection = DriverManager.getConnection(url, user, password);
+
+            if ("".equals(newpass.getText())) {
+                JOptionPane.showMessageDialog(new JFrame(), "Password is require", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            } else if ("".equals(connewpass.getText())) {
+                JOptionPane.showMessageDialog(new JFrame(), "Pls confirm your password", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            } else if (!newpass.getText().equals(connewpass.getText())) {
+                JOptionPane.showMessageDialog(new JFrame(), "Please enter the same password", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            } else {
+                Newpassword = newpass.getText();
+                System.out.println(Newpassword);
+
+                String checkUserQuery = "SELECT * FROM user WHERE username=? and password=?";
+                try (PreparedStatement checkUserStatement = sqlconnection.prepareStatement(checkUserQuery)) {
+                    checkUserStatement.setString(1, Username);
+                    checkUserStatement.setString(2, Newpassword);
+                    ResultSet checkUserResult = checkUserStatement.executeQuery();
+                    if (checkUserResult.next()) {
+                        int countCheck = checkUserResult.getInt(1);
+                        if (countCheck > 0) {
+                            showMessageDialog(null, "Password already exists. Please choose a different password.");
+                            return;
+                        }
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                String updatepassword = "UPDATE user"
+                        + " SET password='" + Newpassword + "' WHERE username='" + Username + "'";
+                System.out.println(Username);
+
+                Statement statement = sqlconnection.createStatement();
+                int rowsUpdated = statement.executeUpdate(updatepassword);
+                if (rowsUpdated > 0) {
+                    System.out.println("Update successful! Rows affected: " + rowsUpdated);
+
+                } else {
+                    System.out.println("No records were updated." + Username);
+                }
+
+                newpass.setText("");
+                connewpass.setText("");
+
+                showMessageDialog(null, "New password has been updated successfully!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_ConfirmActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Back;
+    private javax.swing.JLabel Changepass;
+    private javax.swing.JButton Confirm;
+    private javax.swing.JLabel NewPass;
+    private javax.swing.JLabel confirmpass;
+    private javax.swing.JPasswordField connewpass;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField newpass;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ finalisedPriceTracker/PriceTrend.java
@@ -0,0 +1,231 @@
+package PriceTracker;
+
+import java.awt.Color;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.category.CategoryDataset;
+import com.raven.datechooser.DateChooser;
+//import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.raven.datechooser.DateBetween;
+import com.raven.datechooser.listener.DateChooserAction;
+import com.raven.datechooser.listener.DateChooserAdapter;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.text.SimpleDateFormat;
+import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.showMessageDialog;
+import javax.swing.JPanel;
+
+public class PriceTrend extends javax.swing.JFrame {
+
+    private CategoryDataset pricedata;
+    private DateChooser Pricedays = new DateChooser();
+    public String item;
+    public String unit;
+    public int itemcode;
+    public String Username;
+
+    PriceTrend(int itemcode, String item, String unit, String Username) {
+        initComponents();
+        this.item = item;
+        this.unit = unit;
+        this.itemcode = itemcode;
+        this.Username = Username;
+        Pricedays.setTextField(Startingdate);
+        Pricedays.setDateSelectionMode(DateChooser.DateSelectionMode.BETWEEN_DATE_SELECTED);
+        Pricedays.setSelectedDateBetween(1, 8, 2023, 29, 8, 2023);
+        Pricedays.setLabelCurrentDayVisible(false);
+        Pricedays.addActionDateChooserListener(new DateChooserAdapter() {
+            @Override
+            public void dateBetweenChanged(DateBetween date, DateChooserAction action) {
+                SimpleDateFormat FormatY = new SimpleDateFormat("yyyy-MM-dd");
+                String Firstday = FormatY.format(date.getFromDate());
+                String Lastday = FormatY.format(date.getToDate());
+                showChart(Firstday, Lastday, itemcode);
+            }
+        });
+    }
+
+    class PriceTracker extends JPanel {
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g;
+            int width = getWidth();
+            int height = getHeight();
+
+            Color color1 = new Color(219, 215, 210);
+            Color color2 = new Color(176, 196, 222);
+            GradientPaint gp = new GradientPaint(0, 0, color1, 180, height, color2);
+            g2d.setPaint(gp);
+            g2d.fillRect(0, 0, width, height);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel2 = new PriceTracker();
+        Pricechart = new javax.swing.JPanel();
+        Startingdate = new javax.swing.JTextField();
+        DATE = new javax.swing.JLabel();
+        Back = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
+
+        jPanel2.setBackground(new java.awt.Color(102, 204, 255));
+
+        Pricechart.setBackground(new java.awt.Color(255, 255, 255));
+        Pricechart.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Price Trend", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.TOP));
+        Pricechart.setLayout(new javax.swing.BoxLayout(Pricechart, javax.swing.BoxLayout.LINE_AXIS));
+
+        Startingdate.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(29, 41, 81), new java.awt.Color(29, 41, 81), null, null));
+
+        DATE.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        DATE.setForeground(new java.awt.Color(29, 41, 81));
+        DATE.setText("Date:");
+
+        Back.setText("<< Back");
+        Back.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BackActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addComponent(DATE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(Startingdate, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(Pricechart, javax.swing.GroupLayout.PREFERRED_SIZE, 1178, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(Back)
+                .addGap(20, 20, 20))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Startingdate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(DATE, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(3, 3, 3)
+                .addComponent(Pricechart, javax.swing.GroupLayout.DEFAULT_SIZE, 516, Short.MAX_VALUE)
+                .addGap(16, 16, 16)
+                .addComponent(Back)
+                .addContainerGap())
+        );
+
+        getContentPane().add(jPanel2);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    public void showChart(String Firstday, String Lastday, int itemcode) {
+        String url = "jdbc:mysql://127.0.0.1:3306/pricetracker";
+        String user = "root";
+        String password = "";
+
+        DefaultCategoryDataset pricedata = new DefaultCategoryDataset();
+        double min = 9999, max = 0, buffer = 0.02;
+
+        try (Connection sqlconnection = DriverManager.getConnection(url, user, password)) {
+
+            String avgquery = "SELECT date, ROUND(AVG(price), 2) AS avg "
+                    + "FROM pricecatcher "
+                    + "WHERE date BETWEEN ? AND ? "
+                    + "AND item_code = ? "
+                    + "GROUP BY date;";
+
+            try (PreparedStatement AVG = sqlconnection.prepareStatement(avgquery)) {
+                AVG.setInt(3, itemcode); // Set item code once outside the loop
+                AVG.setString(1, Firstday);
+                AVG.setString(2, Lastday);
+
+                try (ResultSet Avg = AVG.executeQuery()) {
+                    if (Avg.next()) {
+                        do {
+                            double avg = Avg.getDouble("avg");
+                            String date = Avg.getString("date");
+
+                            System.out.println(date + " avg:" + avg);
+
+                            if (avg < min) {
+                                min = avg;
+                                System.out.println("min:" + min);
+                            }
+                            if (avg > max) {
+                                max = avg;
+                                System.out.println("max:" + max);
+                            }
+                            pricedata.addValue(avg, "price for " + date, date);
+                        } while (Avg.next());
+                    } else {
+                        showMessageDialog(null, "Sorry \nThere is no data available for " + item + "", "Item Not Available", JOptionPane.WARNING_MESSAGE);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        JFreeChart jchart = ChartFactory.createBarChart("Price trend", "Date", "Price", pricedata,
+        PlotOrientation.HORIZONTAL, true, true, false);
+        CategoryPlot plot = jchart.getCategoryPlot();
+        plot.setRangeGridlinePaint(Color.black);
+        NumberAxis yAxis = (NumberAxis) plot.getRangeAxis();
+        yAxis.setLowerBound(min - buffer);
+        yAxis.setUpperBound(max + buffer);
+
+        ChartPanel chartPanel = new ChartPanel(jchart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(1170, 500));
+
+        Pricechart.removeAll();
+        Pricechart.add(chartPanel);
+        Pricechart.updateUI();
+        Pricechart.revalidate();
+        Pricechart.repaint();
+    }
+    private void BackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BackActionPerformed
+        UserItem Use = new UserItem(this.item, this.unit, this.Username);
+        Use.setVisible(true);
+        Use.pack();
+        Use.setLocationRelativeTo(null);
+        this.dispose();
+    }//GEN-LAST:event_BackActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Back;
+    private javax.swing.JLabel DATE;
+    private javax.swing.JPanel Pricechart;
+    private javax.swing.JTextField Startingdate;
+    private javax.swing.JPanel jPanel2;
+    // End of variables declaration//GEN-END:variables
+}
